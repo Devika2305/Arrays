136b60dd88fed0612ed771dea157feffe18de8e0
Devika Gupta <57962316+Devika2305@users.noreply.github.com>
2021-05-26 21:30:41 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,25 @@
+package Questions;
+import java.util.*;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int t= s.nextInt();
+		s.nextLine();
+		while(t-->0) {
+			String str=s.nextLine();
+			System.out.println(reverse(str));
+		}
+	}
+	static String reverse(String str) {
+		int n=str.length();
+		if(n==0)
+			return "";
+		return (str.charAt(n-1)+reverse(str.substring(0,n-1)));
+				
+	}
+	
+
+
+}
